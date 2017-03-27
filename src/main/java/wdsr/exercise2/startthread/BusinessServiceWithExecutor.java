6e95e119cc250227dc0825205cb37b72a96fb25d
nonecover <adam.jakomulski@gmail.com>
2017-03-27 23:29:50 +0200

    All exercises done

--- src/main/java/wdsr/exercise2/startthread/BusinessServiceWithExecutor.java
+++ src/main/java/wdsr/exercise2/startthread/BusinessServiceWithExecutor.java
@@ -1,22 +1,30 @@
 package wdsr.exercise2.startthread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class BusinessServiceWithExecutor {
 	private NumericHelper helper;
-	
+	private final ExecutorService executorService;
+
 	public BusinessServiceWithExecutor(NumericHelper helper) {
 		this.helper = helper;
+		executorService = Executors.newSingleThreadExecutor();
 	}
 
 	/**
-	 * Calculates Fibonacci number asynchronously and invokes the callback when result is available.
-	 * This method returns immediately. 
-	 * @param n Which Fibonacci number should be computed.
-	 * @param callback Callback to be invoked when Fibonacci number is found.
+	 * Calculates Fibonacci number asynchronously and invokes the callback when
+	 * result is available. This method returns immediately.
+	 * 
+	 * @param n
+	 *            Which Fibonacci number should be computed.
+	 * @param callback
+	 *            Callback to be invoked when Fibonacci number is found.
 	 */
 	public void computeFibonacci(int n, FibonacciCallback callback) {
-		// TODO Task: execute the logic below using java.util.concurrent.ExecutorService
-		// The ExecutorService should be declared as a field, not a local variable.
-		long value = helper.findFibonacciValue(n);
-		callback.fibonacciComputed(value);
+		executorService.submit(() -> {
+			long value = helper.findFibonacciValue(n);
+			callback.fibonacciComputed(value);
+		});
 	}
 }
