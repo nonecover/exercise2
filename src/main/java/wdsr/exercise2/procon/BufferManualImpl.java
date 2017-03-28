abd7a3bdc2481b20a3aff9d5dfa68b95da44b71c
nonecover <adam.jakomulski@gmail.com>
2017-03-28 02:11:02 +0200

    All exercises are done

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,53 @@
 package wdsr.exercise2.procon;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
- * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
- * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
+ * Task: implement Buffer interface without using any *Queue classes from
+ * java.util.concurrent package. Any combination of "synchronized", *Lock,
+ * *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
+	private static final int CAPACITY = Integer.MAX_VALUE;
+
+	private final Queue<Order> orders = new LinkedList<Order>();
+	private final ReentrantLock lock = new ReentrantLock();
+	private final Condition inNotEmpty = lock.newCondition();
+	private final Condition isNotFull = lock.newCondition();
+
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+		lock.lockInterruptibly();
+		try {
+			waitTillFull();
+			orders.add(order);
+			inNotEmpty.signal();
+		} finally {
+			lock.unlock();
+		}
 	}
-	
+
+	private void waitTillFull() throws InterruptedException {
+		while (orders.size() == CAPACITY)
+			isNotFull.await();
+	}
+
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		lock.lockInterruptibly();
+		try {
+			waitTillEmpty();
+			Order order = orders.remove();
+			isNotFull.signal();
+			return order;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	private void waitTillEmpty() throws InterruptedException {
+		while (orders.isEmpty())
+			inNotEmpty.await();
 	}
 }
--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -1,15 +1,24 @@
 package wdsr.exercise2.procon;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
- * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
+ * Task: implement Buffer interface using one of *Queue classes from
+ * java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
-	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+	private final BlockingQueue<Order> queue;
+
+	public BufferQueueImpl() {
+		queue = new LinkedBlockingQueue<>();
 	}
-	
+
+	public void submitOrder(Order order) throws InterruptedException {
+		queue.put(order);
+	}
+
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		return queue.take();
 	}
 }
